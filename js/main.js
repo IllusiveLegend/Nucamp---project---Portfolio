da73048d4f76788290a199f826fc5d7e0d550fbe
Nathan Ripple <nripple6@gmail.com>
2022-04-16 10:29:24 -0500

    setting up online repo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
+
+$(function () {
+    $(".carousel").carousel({interval: 6000});
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("$carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+
+ 
+
